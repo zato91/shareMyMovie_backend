783e8105045f622ac3ad027c355f46395719fa72
zato91 <zatokey@gmail.com>
2020-09-05 21:52:37 -0400

    create movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,25 @@
 class MoviesController < ApplicationController
+
+    def create
+        list_movie = ListMovie.find_by(category: params["existing_list"])
+        list_c = ListMovie.create(category:params["list_movie"])
+        if list_c.present?
+        movie = Movie.new(name:params["name"], picture: params["picture"], 
+            rating: params[:rating].to_i, descritpion: params["description"], extra_info: params["extra_info"],
+            extra_url: params["extra_url"], list_movie_id: list_c.id)
+        else 
+            movie = Movie.new(name:params["name"], picture: params["picture"], 
+                rating: params[:rating].to_i, descritpion: params["description"], extra_info: params["extra_info"],
+                extra_url: params["extra_url"], list_movie_id: list_movie.id)
+        end
+        byebug
+    end 
 end
+
+# t.string "name"
+# t.string "picture"
+# t.integer "rating"
+# t.string "description"
+# t.string "extra_info"
+# t.string "extra_url"
+# t.integer "list_movie_id"
--- app/models/list_movie.rb
+++ app/models/list_movie.rb
@@ -2,4 +2,6 @@ class ListMovie < ApplicationRecord
     has_many :user_list_movies
     has_many :users, through: :user_list_movies
     has_many :movies
+
+    validates :category, presence: true
 end
