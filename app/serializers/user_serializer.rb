4b07a0f3536799daa3d90ab6dda545e061ba145f
zato91 <zatokey@gmail.com>
2020-09-07 23:58:54 -0400

    fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,8 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
 
   has_many :list_movies
-<<<<<<< HEAD
   has_many :movies, through: :list_movies
-=======
->>>>>>> b8d45916cc7361a8e4828bded7c3b669c0ead6e9
 end
