a3e10ae7901144e180c7cf00c2ca0315584dd014
zato91 <zatokey@gmail.com>
2020-09-09 02:58:59 -0400

    new branch changing friendhsip table

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,14 @@ m = Movie.create(name:"film test", picture: "https://utellyassets9-1.imgix.net/a
     extra_url: "yep", list_movie: l)
 
     UserListMovie.create(user:User.find(1),list_movie:l)
+
+    # e  =ListMovie.create(category:"Manga")
+    # t = ListMovie.create(category:"Horror")
+    # d = ListMovie.create(category:"Action")
+    # z = ListMovie.create(category:"Drama")
+
+
+    # UserListMovie.create(user:User.find(1),list_movie:e)
+    # UserListMovie.create(user:User.find(1),list_movie:t)
+    # UserListMovie.create(user:User.find(1),list_movie:d)
+    # UserListMovie.create(user:User.find(1),list_movie:z)
