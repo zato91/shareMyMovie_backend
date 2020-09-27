ab6bb72d09aefcf648a806ddf38c13cc20a708ed
zato91 <zatokey@gmail.com>
2020-09-26 23:16:14 -0400

    added creation list

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,20 +7,20 @@ class MoviesController < ApplicationController
     def create
         list_movie = ListMovie.find_by(category: params["existing_list"])
         user = User.find(params[:id].to_i)
-        # list_c = ListMovie.create(category:params["list_movie"])
-        # if list_c.present?
-        # movie = Movie.new(name:params["name"], picture: params["picture"], 
-        #     rating: params[:rating].to_i, description: params["description"], extra_info: params["extra_info"],
-        #     extra_url: params["extra_url"], list_movie_id: list_c.id)
-        #     join = list_movie.user_list_movies.build(user_id:user.id, list_movie_id:list_movie.id)
-        #     join.save
-        # else 
+        list_c = ListMovie.create(category:params["list_movie"])
+        if list_c.present?
+        movie = Movie.new(name:params["name"], picture: params["picture"], 
+            rating: params[:rating].to_i, description: params["description"], extra_info: params["extra_info"],
+            extra_url: params["extra_url"], list_movie_id: list_c.id)
+            join = list_movie.user_list_movies.build(user_id:user.id, list_movie_id:list_movie.id)
+            join.save
+        else 
         
-        #  list_movie.present?
+         list_movie.present?
             movie = Movie.new(name:params["name"], picture: params["picture"], 
                 rating: params[:rating].to_i, description: params["description"], extra_info: params["extra_info"],
                 extra_url: params["extra_url"], list_movie_id: list_movie.id)
-        #  end
+         end
 
          if movie.valid?
             movie.save
