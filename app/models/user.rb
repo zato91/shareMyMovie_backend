deab461e6fb745cac4f4e3423df04d179bcdc5e3
zato91 <zatokey@gmail.com>
2020-09-29 23:30:17 -0400

    friendship relationship user constroller

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,12 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true
 
+
+    has_many :followed_friendships, foreign_key: :friend_id, class_name: 'Friendship'
+    has_many :followers, through: :followed_friendships
+
+    has_many :initiated_follows, foreign_key: :user_id, class_name: 'Friendship'
+    has_many :followees, through: :initiated_follows
+
     has_secure_password
 end
