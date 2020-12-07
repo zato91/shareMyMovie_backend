5f7f15a3eb4e83458149b8e3e7ff2b88463e6c39
zato91 <zatokey@gmail.com>
2020-12-07 18:37:00 -0500

    rspec gem and create rspec userModel file

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context "validation tests" do
+
+    it 'ensure userName presence' do
+    end
+
+    it 'ensure e-name presence'do
+    end
+    
+ 
+
+  end 
+end
--- test/controllers/list_movies_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ListMoviesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
