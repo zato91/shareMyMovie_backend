c4133d3ba7df1f2b9001ad59356844ce44eed57f
zato91 <zatokey@gmail.com>
2020-12-07 18:46:12 -0500

    .rspec added format documentation and create 1st test username validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,19 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context "validation tests" do
 
-    it 'ensure userName presence' do
+    it 'ensure username presence' do
+
+      user = User.new(username: "test").save
+      expect(user).to eq(false)
     end
 
-    it 'ensure e-name presence'do
-    end
+    # it 'ensure password presence'do
+      # user = User.new(username: "test").save
+      # expect(user).to eq(false)
+    # end
     
+    # it 'ensure username uniq' do
+    # end 
  
 
   end 
