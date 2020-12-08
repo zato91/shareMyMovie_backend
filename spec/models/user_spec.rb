b29f872a83cb4d7f7e9c73f105f1c502ab319e07
zato91 <zatokey@gmail.com>
2020-12-07 19:00:10 -0500

    testing username&password validation and username uniquness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,17 +5,20 @@ RSpec.describe User, type: :model do
 
     it 'ensure username presence' do
 
-      user = User.new(username: "test").save
+      user = User.new(password: "test").save
       expect(user).to eq(false)
     end
 
-    # it 'ensure password presence'do
-      # user = User.new(username: "test").save
-      # expect(user).to eq(false)
-    # end
+    it 'ensure password presence'do
+      user = User.new(username: "test").save
+      expect(user).to eq(false)
+    end
     
-    # it 'ensure username uniq' do
-    # end 
+   
+    before { User.create!(username: 'john@home.xyz', password:'test') }
+      it 'is invalid if the email is not unique' do
+      expect(subject).to be_invalid
+    end
  
 
   end 
